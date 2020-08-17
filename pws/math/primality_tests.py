5d8326016fb49529bf8d36f4e7b820543b26615f
David <bpf.parser@gmail.com>
2020-08-18 00:37:26 +0200

    add small divisor primality test

--- pws/math/primality_tests.py
+++ pws/math/primality_tests.py
@@ -15,13 +15,28 @@ class PrimalityType(Enum):
 
     COMPOSITE = 0
     PROBABLY_PRIME = 1
-    PRIME = 2
+    POSSIBLY_PRIME = 2
+    
+    PRIME = 3
     
     # for 1
-    NEITHER = 3
+    NEITHER = 4
 
 
-def fermat(n: int, rounds: int=2) -> PrimalityType:
+def division_test(n: int):
+    """
+    Small compositeness test by dividing by the first 20 primes.
+    """
+
+    primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]
+    
+    for p in primes:
+        if (n / p).is_integer():
+            return PrimalityType.COMPOSITE
+
+    return PrimalityType.POSSIBLY_PRIME
+
+def fermat_test(n: int, rounds: int=2) -> PrimalityType:
     """
     The Fermat compositeness test is a very performant and lightweight test.
     
@@ -61,7 +76,7 @@ def fermat(n: int, rounds: int=2) -> PrimalityType:
     
     return PrimalityType.PROBABLY_PRIME
 
-def miller_rabin(n: int, rounds: int = 32):
+def miller_rabin_test(n: int, rounds: int = 32):
     
     if n in (2, 3):
         return PrimalityType.PRIME
@@ -97,4 +112,4 @@ def miller_rabin(n: int, rounds: int = 32):
 
     return PrimalityType.PROBABLY_PRIME
 
-# TODO lucas test 
+# TODO lucas test, erastothenes sieve
