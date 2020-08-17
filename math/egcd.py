7c18319a7d435300a7e5d960f28e39ba85a5445e
David <bpf.parser@gmail.com>
2020-08-17 04:47:01 +0200

    correctly associate computed coefficients with arguments

--- math/egcd.py
+++ math/egcd.py
@@ -6,9 +6,11 @@ def egcd(a: int, b: int) -> Tuple[int, int, int]:
     Calculates the Greatest Common Divisor (gcd) of the integers `a`, and `b`. 
     Additionally, calculates the Bézout coefficients `x`, `y` associated with a and b, all using the extended Euclidean algorithm
     """
-
+    
+    swapped = False
     if a < b:
         a, b = b, a
+        swapped = True
 
     def _recurse_egcd(a: int, b: int):
         """Small stub so the unnecessary compare/swap does not occur for recursion"""
@@ -23,7 +25,18 @@ def egcd(a: int, b: int) -> Tuple[int, int, int]:
         # Recursively "fill up" bezout coefficients.
         x = y_0 - (b // a) * x_0
         y = x_0
-
+        
         return (gcd, x, y)
     
-    return _recurse_egcd(a, b)
+
+
+    gcd, x, y =  _recurse_egcd(a, b)
+    
+    # If a swap took place previously,
+    # we need to swap the associated coefficients as well.
+    
+    if swapped:
+        return gcd, y, x
+    else:
+        return gcd, x, y
+
