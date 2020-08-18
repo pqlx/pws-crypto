3e87710644b29d5cf290af4ec7024f597cb84d9d
David <bpf.parser@gmail.com>
2020-08-18 02:22:40 +0200

    Slightly tweak keypair generation method name depending on module

--- pws/asymmetric/__init__.py
+++ pws/asymmetric/__init__.py
@@ -1,1 +1,1 @@
-from pws.asymmetric.rsa import RSAPublicKey, RSAPrivateKey, RSAKeyPair, rsa_generate_keypair
+from pws.asymmetric.rsa import RSAPublicKey, RSAPrivateKey, RSAKeyPair, generate_keypair as rsa_generate_keypair
--- pws/asymmetric/rsa/__init__.py
+++ pws/asymmetric/rsa/__init__.py
@@ -1,2 +1,2 @@
 from pws.asymmetric.rsa.keys import RSAPublicKey, RSAPrivateKey, RSAKeyPair
-from pws.asymmetric.rsa.keygen import generate_keypair as rsa_generate_keypair
+from pws.asymmetric.rsa.keygen import generate_keypair
