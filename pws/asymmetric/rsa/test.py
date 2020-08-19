471bd932566df1e789126aaa0f4a9c3d9c015249
David <bpf.parser@gmail.com>
2020-08-19 03:21:12 +0200

    add signature + verification tests

--- pws/asymmetric/rsa/test.py
+++ pws/asymmetric/rsa/test.py
@@ -5,6 +5,7 @@ from pwn import hexdump
 from datetime import datetime
 import secrets
 import random
+import time
 
 def do_test(key_size: int=2048, **kwargs):
     print("[*] Testing RSA")
@@ -23,8 +24,8 @@ def do_test(key_size: int=2048, **kwargs):
     print(f"[*] Generation took {ms_elapsed} ms..")
     print(80*"-")
     print()
-    n_blobs = kwargs.get("n_blobs") or 8
-    blob_range = kwargs.get("blob_range") or (key_size // (8 * 4), key_size // (8 *2))
+    n_blobs = kwargs.get("n_blobs", 8)
+    blob_range = kwargs.get("blob_range", (key_size // (8 * 4), key_size // (8 *2)))
     
     
     print(f"[*] Generating {n_blobs} blobs of data with length inbetween {blob_range} (in bytes)")
@@ -34,35 +35,81 @@ def do_test(key_size: int=2048, **kwargs):
     print("[+] Done generating blobs!")
     print()
     
-    pad_type = kwargs.get("pad_type") or "pkcs1"
+    encrypt_pad_type = kwargs.get("encryption_pad_type", "pkcs1")
 
-    print(f"[*] Proceeding to encrypt and subsequently decrypt {n_blobs} blobs using padding scheme: {pad_type}") 
+    print(f"[*] Proceeding to encrypt and subsequently decrypt {n_blobs} blobs using padding scheme: {encrypt_pad_type}") 
     print("-"*80)
     print()
+    
+    time.sleep(1)
+    
+    n_encrypt_success = 0
     for i, blob in enumerate(blobs):
         
         print(f"[*] Blob {i}:")
         print(hexdump(blob))
         print()
 
-        encrypted = kp.pub.encrypt(blob, pad_type=pad_type)
+        encrypted = kp.pub.encrypt(blob, pad_type=encrypt_pad_type)
 
         print("[*] Encrypted:")
         print(hexdump(encrypted)) 
         print()
         
-        decrypted = kp.priv.decrypt(encrypted, pad_type=pad_type)
+        decrypted = kp.priv.decrypt(encrypted, pad_type=encrypt_pad_type)
         
         print("[*] Decrypted:")
         print(hexdump(decrypted))
 
         if decrypted == blob:
-            print("[+] Success! m == decrypt(encrypt(m)) !")
+            print("[+] Success! m == decrypt(encrypt(m))")
+            n_encrypt_success += 1
         else:
             print("[x] Fail! m != decrypt(encrypt(m))")
         print("-"*80)
         print()
+        
+        time.sleep(0.5)
 
+    sign_pad_type = kwargs.get("signature_pad_type", "pss")
+    
+    print(f"[*] Proceeding to sign a subsequently verify {n_blobs} blobs using padding scheme: {sign_pad_type}")
+    print("-"*80)
+    print()
+    
+    time.sleep(1)
+    
+    n_signature_success = 0
+    for i, blob in enumerate(blobs):
+
+        print(f"[*] Blob {i}:")
+        print(hexdump(blob))
+        print()
+
+        signature = kp.priv.sign(blob, pad_type=sign_pad_type)
+
+        print("[*] Signed:")
+        print(hexdump(signature))
+        print()
+
+        print("[*] Verifying signature:")
+        verified = kp.pub.verify(blob, signature, pad_type=sign_pad_type)
+        
+        if verified:
+            print("[+] Success! Signature verified correctly.")
+            n_signature_success += 1
+        else:
+            print("[x] Fail! Signature failed verification.")
+        
+        print("-"*80)
+        print()
+        time.sleep(0.5)
+    
+    print("Results:")
+    print("-"*80)
+    print(f"{n_encrypt_success}/{n_blobs} blobs successfully encrypted + decrypted (padding scheme: {encrypt_pad_type})")
+    print(f"{n_signature_success}/{n_blobs} blobs successfully signed + verified (padding scheme: {sign_pad_type})")
+    
 if __name__ == "__main__":
     import argparse
     
@@ -70,9 +117,11 @@ if __name__ == "__main__":
             description="RSA Testing module",
             formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     
-    parser.add_argument("--pad", "-p", choices=["pkcs1", "oaep", "none"], default="pkcs1", type=str, help="Padding mode to use for encryption") 
+    parser.add_argument("--encryption-padding", choices=["pkcs1", "oaep", "none"], default="pkcs1", type=str, help="Padding mode to use for encryption") 
+    parser.add_argument("--signature-padding", choices=["pss", "none"], default="pss", type=str, help="Padding mode to use for signatures")
     parser.add_argument("--keysize", "-n", default=2048, type=int, help="Key size to generate (in bits). Should be divisible by eight.")
     parser.add_argument("--blobs", "-b", default=8, type=int, help="Amount of random plaintext blobs to generate.:")
     args = parser.parse_args()
-
-    do_test(key_size=args.keysize, n_blobs=args.blobs, pad_type=args.pad)
+    
+    print(args)
+    do_test(key_size=args.keysize, n_blobs=args.blobs, encryption_pad_type=args.encryption_padding, signature_pad_type=args.signature_padding)
