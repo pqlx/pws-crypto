77dad6ae31569e471ab4ad64135de11cf55036b3
pqlx <bpf.parser@gmail.com>
2021-01-07 23:22:48 +0100

    add WIP elliptic curves over finite fields

---
+++ pws/math/ec.py
@@ -0,0 +1,118 @@
+from pws.math import modinv
+
+class ECurvePoint:
+    
+    def __init__(self, curve: 'ECurve', x, y):
+        self.curve = curve
+        self.x = x
+        self.y = y
+
+    def copy(self):
+        return ECurvePoint(self.curve, self.x, self.y)
+
+    def __neg__(self):
+        new_x = self.x
+        new_y = (-self.y % self.curve.modulus)
+
+        point = ECurvePoint(self.curve, new_x, new_y)
+
+        assert self.curve.is_element(point)
+
+        return point
+    
+    def __add__(self, other):
+        if self == self.curve.identity:
+            return other.copy()
+        
+        if other == self.curve.identity:
+            return self.copy()
+        
+        p = self.curve.modulus
+
+        x1, y1 = self
+        x2, y2 = other
+
+        if x1 == x2 and y1 == (-y2 % self.curve.modulus):
+            return self.curve.identity.copy()
+        
+        # calculate the slope
+        if self != other:
+            l = (((y2 - y1) % p) * modinv( x2 - x1 , p)[0]) % p
+        else:
+            l = (((3 * pow(x1, 2, p) + self.curve.a) % p) * modinv(2 * y1, p)[0]) % p
+        
+        x3 = (pow(l, 2, p) - x1 - x2) % p
+        y3 = ((l * (x1 - x3) ) - y1) % p
+
+        point = ECurvePoint(self.curve, x3, y3)
+        
+        assert self.curve.is_element(point)
+
+        return point
+
+    def __eq__(self, other: 'ECurvePoint'):
+        assert self.curve == other.curve
+        return self.x == other.x and self.y == other.y
+    
+    def __iter__(self):
+        return (_ for _ in (self.x, self.y))
+
+    def __repr__(self):
+        return f"ECurvePoint(curve={repr(self.curve)}, x={self.x}, y={self.y})"
+    
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+class ECurveIdentity(ECurvePoint):
+
+    def __init__(self, curve: 'ECurve'):
+
+        super(ECurveIdentity, self).__init__(curve, float("inf"), float("inf"))
+
+    def copy(self):
+        return ECuveIdentity(self.curve)
+
+    def __neg__(self):
+        return self.copy()
+    
+    def __eq__(self, other: 'ECurvePoint'):
+        assert self.curve == other.curve
+        return type(other) == ECurveIdentity
+    
+    def __iter__(self):
+        return None
+
+    def __repr__(self):
+        return f"ECurveIdentity(curve={repr(self.curve)})"
+
+    def __str__(self):
+        return f"Identity element O (point at infinity)"
+
+class ECurve:
+    
+    """Represents an elliptic curve defined over a finite integer field F_p"""
+    def __init__(self, a: int, b: int, p: int):
+        self.a = a
+        self.b = b
+        self.modulus = p
+        self.identity = ECurveIdentity(self)
+
+    def is_element(self, point: ECurvePoint):
+        p = self.modulus
+
+        return pow(point.y, 2, p) == ((pow(point.x, 3, p) + (self.a * point.x) % p + self.b) % p)
+
+    def point(self, x: int, y: int):
+        point = ECurvePoint(self, x, y)
+
+        assert self.is_element(point)
+        return point
+    
+    def __eq__(self, other: 'ECurve'):
+        return self.a == other.a and self.b == other.b and self.modulus == other.modulus
+
+    def __repr__(self):
+        return f"ECurve(a={self.a}, b={self.b}, p={self.modulus})"
+
+    def __str__(self):
+        return f"Elliptic curve y^2 = x^3 + {self.a}x + {self.b} (defined over finite field F_p = {{0, 1, ..., {self.modulus-1}}})"
